3f4bd32191c922d1ef71b793a9dbf578f6d354bc
ivan-sosorin <i.sosorin@mindbox.cloud>
2025-04-08 13:21:24 +0300

    remove split json and deserializing model validation

--- Mindbox.YandexTracker/Extensions/HttpResponseMessageExtensions.cs
+++ Mindbox.YandexTracker/Extensions/HttpResponseMessageExtensions.cs
@@ -30,57 +30,26 @@ internal static class HttpResponseMessageExtensions
 	/// <typeparam name="TResult"> Тип, в который необходимо десериализовать из HttpResponseMessage Content. </typeparam>
 	/// <returns> Десериализованное значение </returns>
 	/// <exception cref="YandexTrackerException" >Исключение выбрасывается,
-	/// когда пришёл невалидный JSON с YandexTracker API или к. </exception>
+	/// когда пришёл невалидный JSON с YandexTracker API. </exception>
 	internal static async Task<TResult?> DeserializeYandexTrackerResponseContentAsync<TResult>(
 		this HttpResponseMessage httpResponse,
 		CancellationToken cancellationToken = default)
 	{
 		TResult result;
 		var resultContent = await httpResponse.Content.ReadAsStringAsync(cancellationToken);
-		var isValidJson = TryParseDocument(resultContent, out var jsonDocument, out var exception);
-
-		if (!isValidJson)
-			throw new YandexTrackerException(
-				$"Invalid JSON was provided by YandexTracker API: {resultContent}",
-				HttpStatusCode.BadGateway,
-				exception);
 
 		try
 		{
-			result = jsonDocument!
-				.Deserialize<TResult>(YandexTrackerConstants.YandexTrackerJsonSerializerOptions)!;
+			result = JsonSerializer
+				.Deserialize<TResult>(resultContent, YandexTrackerConstants.YandexTrackerJsonSerializerOptions)!;
 		}
 		catch (JsonException jsonException)
 		{
 			throw new YandexTrackerException(
-				$"Invalid type was provided {typeof(TResult).Name} for deserialization JSON" +
-				$" from YandexTracker API: {resultContent}",
+				$"Invalid JSON was provided by YandexTracker API: {resultContent}",
 				HttpStatusCode.InternalServerError, jsonException);
 		}
 
 		return result;
-	}
-
-	/// <summary>
-	/// Метод для валидации и получения JsonDocument из строки.
-	/// </summary>
-	/// <param name="json"> Строка, которая представляет из себя JSON. </param>
-	/// <param name="document"> Результат распаршивания строки. </param>
-	/// <param name="exception"> <see cref="JsonException"/> для случая, когда распарсить JSON не удалось. </param>
-	/// <returns> Результат попытки распарсить JSON строку. </returns>
-	private static bool TryParseDocument(string json, out JsonDocument? document, out JsonException? exception)
-	{
-		try
-		{
-			document = JsonDocument.Parse(json);
-			exception = null;
-			return true;
-		}
-		catch (JsonException jsonException)
-		{
-			document = null;
-			exception = jsonException;
-			return false;
-		}
 	}
 }
